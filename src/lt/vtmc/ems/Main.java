cea5c23f261853d880af8e9fdeb57141c22e9305
Kestutis Kurienius <kurkestutis@gmail.com>
2020-05-15 19:28:13 +0300

    Main.java added missed 'static'

--- src/lt/vtmc/ems/Main.java
+++ src/lt/vtmc/ems/Main.java
@@ -1,7 +1,7 @@
 package lt.vtmc.ems;
 
 public class Main {
-    public void main(String... args) {
+    public static void main(String... args) {
         Menu menu = new Menu();
         menu.start();
     }
