3a41771824334e64afd9fa9b1f4ae0c91826a8a3
Kestutis Kurienius <kurkestutis@gmail.com>
2020-05-20 15:51:03 +0300

    Program starts greeting added

--- src/lt/vtmc/ems/Main.java
+++ src/lt/vtmc/ems/Main.java
@@ -2,6 +2,7 @@ package lt.vtmc.ems;
 
 public class Main {
     public static void main(String... args) {
+		System.out.println("Hello!\nThe program starts working");
         Menu menu = new Menu();
         menu.start();
     }
